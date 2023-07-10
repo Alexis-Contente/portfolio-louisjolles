83fe88011e2876f93143441c1af797959aa0bd43
Alexis-Contente <alexis.contente@gmail.com>
2023-07-10 11:28:38 +0200

    Portfolio Responsive

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -2,27 +2,28 @@ import styles from "../../../public/style/portfolio.module.css";
 import Image from "next/image";
 import Musics from "../../../public/data/images/microphone.jpg";
 import Photos from "../../../public/data/images/appareil_photo.jpg";
+import Link from "next/link";
 
 export default function Portfolio() {
   return (
     <>
       <div className={styles.portfolio__container}>
-        <div className={styles.musics__container}>
+        <Link className={styles.musics__container} href={"/categorie-musics"}>
           <p className={styles.categorie}>Musics</p>
           <Image
             className={styles.image}
             src={Musics}
             alt="Image d'un microphone"
           ></Image>
-        </div>
-        <div className={styles.photos__container}>
+        </Link>
+        <Link className={styles.photos__container} href={"/photos"}>
           <p className={styles.categorie}>Photos</p>
           <Image
             className={styles.image}
             src={Photos}
             alt="Image de deux appareils photos"
           ></Image>
-        </div>
+        </Link>
       </div>
     </>
   );
