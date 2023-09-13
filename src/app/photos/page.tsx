6743507e662a23dda7ba4545776cadb8862a94f3
Alexis-Contente <alexis.contente@gmail.com>
2023-09-13 16:10:43 +0200

    display photos done

--- src/app/photos/page.tsx
+++ src/app/photos/page.tsx
@@ -1,24 +1,28 @@
 "use client";
-
-import Image from "next/image";
 import styles from "../../../public/style/photos.module.css";
+import Image from "next/image";
 import database from "../../db.json";
+
+const images = database.photos;
 
 export default function Photos() {
   return (
-    <div className={styles.photos_container}>
-      {database.photos.map((photo) => (
-        <Image
-          id={`photo${photo.id}`}
-          className={styles.item}
-          key={photo.id}
-          src={photo.src}
-          alt="photos"
-          width={500}
-          height={500}
-          loading="lazy"
-        ></Image>
-      ))}
-    </div>
+    <>
+      <div className={styles.photo__container}>
+        <div className={styles.photo}>
+          {images.map((image) => (
+            <Image
+              className={styles.item}
+              key={image.id}
+              src={image.src}
+              alt="Photos"
+              width={image.width}
+              height={image.height}
+              loading="lazy"
+            />
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
