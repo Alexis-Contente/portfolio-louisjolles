c572ad6aa3d68a13c1a394875da0ea2be3633d95
Alexis-Contente <alexis.contente@gmail.com>
2023-09-14 13:03:42 +0200

    Modal photo created, display photos on modal with next and previous arrow

--- src/app/photos/page.tsx
+++ src/app/photos/page.tsx
@@ -1,11 +1,39 @@
 "use client";
 import styles from "../../../public/style/photos.module.css";
 import Image from "next/image";
+import Close from "../../../public/assets/icons8-close-window-48.png";
+import Next from "../../../public/assets/icons8-arrow-next-50.png";
+import Prev from "../../../public/assets/icons8-arrow-prev-50.png";
 import database from "../../db.json";
+import { useState } from "react";
 
 const images = database.photos;
 
 export default function Photos() {
+  const [selectedImageId, setSelectedImageId] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClick = (id) => {
+    setSelectedImageId(id);
+    setShowModal(true);
+  };
+
+  const handlePrevClick = () => {
+    const currentIndex = images.findIndex(
+      (image) => image.id === selectedImageId
+    );
+    const newIndex = (currentIndex - 1 + images.length) % images.length;
+    setSelectedImageId(images[newIndex].id);
+  };
+
+  const handleNextClick = () => {
+    const currentIndex = images.findIndex(
+      (image) => image.id === selectedImageId
+    );
+    const newIndex = (currentIndex + 1) % images.length;
+    setSelectedImageId(images[newIndex].id);
+  };
+
   return (
     <>
       <div className={styles.photo__container}>
@@ -19,10 +47,51 @@ export default function Photos() {
               width={image.width}
               height={image.height}
               loading="lazy"
+              onClick={() => handleClick(image.id)}
             />
           ))}
         </div>
       </div>
+
+      {showModal && selectedImageId !== null && (
+        <div className={styles.modal__container}>
+          <div className={styles.modal}>
+            <Image
+              className={styles.modal__prev}
+              alt="Previous arrow"
+              width={30}
+              height={30}
+              src={Prev}
+              onClick={handlePrevClick}
+            />
+            <Image
+              className={styles.modal__image}
+              src={images.find((image) => image.id === selectedImageId).src}
+              alt="Photos"
+              width={500}
+              height={500}
+              loading="lazy"
+            />
+            <Image
+              className={styles.modal__next}
+              alt="Next arrow"
+              width={30}
+              height={30}
+              src={Next}
+              onClick={handleNextClick}
+            />
+            <Image
+              src={Close}
+              alt="Close"
+              width={30}
+              height={30}
+              loading="lazy"
+              className={styles.modal__close}
+              onClick={() => setShowModal(!showModal)}
+            />
+          </div>
+        </div>
+      )}
     </>
   );
 }
